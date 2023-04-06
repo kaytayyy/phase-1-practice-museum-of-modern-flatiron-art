ab8796aebfd7a8a52ec1f709ec5490e1fdcd3282
Inda Smith <inda.k.smith@gmail.com>
2023-04-06 09:58:35 -0400

    need to continue working on this

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('Write your code here');
+const baseUrl = 'http://localhost:3000/'
+const exhibitUrl = baseUrl + 'current-exhibits'
+
+const fetchArt = () =>
+fetch(exhibitUrl)
+.then(response = response.json())
+.then(artData => renderArt(artData))
+
+fetchArt()
+
+const renderArt = art => {
+    renderArtDetails(art[0])
+    const artTitleH2 = document.getElementById('exhibit-title')
+
+    const artTicketsBought = document.getElementById('tickets-bought')
+
+    const artExhibitDescription = document.getElementById('exhibit-description')
+
+    const artComments = document.getElementById('comments-section')
+
+}
